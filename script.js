8b579859f2a5eb5e6ba502e5e6c1f069fbec531c
JohannesOguz <johannesoguz1@outlook.com>
2024-10-01 16:46:04 +0200

    visar länderna från land.json i en lista

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+//Anväder Promise.all för att hämta json filerna
 Promise.all([
-    fetch("land.json").then((res) => res.json()),
-    fetch("stad.json").then((res) => res.json())
+  fetch("land.json").then((res) => res.json()),
+  fetch("stad.json").then((res) => res.json()),
 ])
+
 .then((data) => {
-    console.log("data", data);
-})
+  console.log("data", data);
+
+  const land = data[0];
+  const stad = data[1];
+
+  let ul = document.createElement("ul");
+
+  land.forEach((land) => {
+    let li = document.createElement("li");
+    li.innerText = land.countryname;
+    li.style.fontWeight = "bold";
+
+    ul.appendChild(li);
+
+  });
+
+  document.getElementById("root").appendChild(ul);
+
+});
